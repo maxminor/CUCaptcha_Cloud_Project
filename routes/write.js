305a8f5512cc1151442d1695c48f4352ae24ed91
Thanthawat Suwannachatkul <peace_under@hotmail.com>
2018-04-29 20:14:00 +0700

    add loadimage route

--- routes/write.js
+++ routes/write.js
@@ -1,55 +1,98 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 var AWS = require('aws-sdk')
 AWS.config.update({ region: 'ap-northeast-1' })
 var s3 = new AWS.S3();
 var rekognition = new AWS.Rekognition()
 
-/* GET write description. */
-router.get('/', async function(req, res, next) {
-  var params = {
-    Bucket: 'ebainternshiprekognitionimage',
-    Prefix: 'img',
-  }
-  s3.listObjects(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else {
-      //console.log(data)
-      let imgCnt = Object.keys(data.Contents).length
-      console.log("Image count: ", imgCnt)
-      let rngImg = data.Contents[Math.floor(Math.random() * imgCnt)].Key
-      console.log("Random Image: ", rngImg)
-      let rekogInfo = await rekog(rngImg)
-      console.log(rekogInfo)
-      console.log(rekogInfo.Labels)
-      let imgInfo = {
-        Key: rngImg,
-        Labels: rekogInfo.Labels
-      }
-      res.send(rngImg)
-    }
-  })
-})
+var filejson = JSON.parse(fs.readFileSync('./secret/newfilenames.json'))
 
-const rekog = async (imageName) => {
+
+router.get('/loadimage', (req, res) => {
+  let filenames = filejson.Images;
+
+  console.log(filenames);
+
+  let file = filenames[Math.floor(Math.random() * filenames.length)]
+
+  console.log(file);
+
+  res.render('write', {filename: file});  
+});
+
+router.post('/loadimage', async (req, res) => {
+  console.log(req.body);
+
+  //subject to change
+  const {filename, answer1, answer2, answer3} = req.body;
+
   var params = {
     Image: {
       S3Object: {
         Bucket: 'ebainternshiprekognitionimage',
-        Name: imageName
+        Name: filename
       }
     },
     MaxLabels: 123,
     MinConfidence: 50
+  };
+
+  try {
+    let data = await rekognition.detectLabels(params);
+  } catch (e) {
+    console.log(e);
   }
-  await rekognition.detectLabels(params, function(err, data) {
-    if (err) console.log(err, err.stack)
-    else {
-      console.log(data)
-      return data
-    }
-  })
-}
+});
+
+
+  
+
+// /* GET write description. */
+// router.get('/', async function(req, res, next) {
+//   var params = {
+//     Bucket: 'ebainternshiprekognitionimage',
+//     Prefix: 'img',
+//   }
+//   s3.listObjects(params, function(err, data) {
+//     if (err) console.log(err, err.stack); // an error occurred
+//     else {
+//       //console.log(data)
+//       let imgCnt = Object.keys(data.Contents).length
+//       console.log("Image count: ", imgCnt)
+//       let rngImg = data.Contents[Math.floor(Math.random() * imgCnt)].Key
+//       console.log("Random Image: ", rngImg)
+//       let rekogInfo = await rekog(rngImg)
+//       console.log(rekogInfo)
+//       console.log(rekogInfo.Labels)
+//       let imgInfo = {
+//         Key: rngImg,
+//         Labels: rekogInfo.Labels
+//       }
+//       res.send(rngImg)
+//     }
+//   })
+// })
+
+// const rekog = async (imageName) => {
+//   var params = {
+//     Image: {
+//       S3Object: {
+//         Bucket: 'ebainternshiprekognitionimage',
+//         Name: imageName
+//       }
+//     },
+//     MaxLabels: 123,
+//     MinConfidence: 50
+//   }
+//   await rekognition.detectLabels(params, function(err, data) {
+//     if (err) console.log(err, err.stack)
+//     else {
+//       console.log(data)
+//       return data
+//     }
+//   })
+// }
 
 module.exports = router;
