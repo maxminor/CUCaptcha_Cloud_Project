80306a93756125827e6b62a94df21debca4af509
Sivakorn Chanpitayanukulkij <ch.sivakorn@gmail.com>
2018-05-01 21:39:01 +0700

    select

--- routes/select.js
+++ routes/select.js
@@ -10,20 +10,22 @@ var rekognition = new AWS.Rekognition()
 
 var filejson = JSON.parse(fs.readFileSync('./secret/newfilenames.json'))
 
-var keyLabel = ["Car", "Tree", "Building"]
+var keyLabel = ["Car", "Building"]
 
 router.get('/', async (req, res) => {
   let urllink = 'https://s3-ap-northeast-1.amazonaws.com/ebainternshiprekognitionimage/img/';
   let correctIncorrect = getRandom(keyLabel, 2)
   console.log('YES', correctIncorrect[0])
   console.log('NO', correctIncorrect[1])
+
+  //Getting correct image
   let params = {
     ExpressionAttributeValues: {
       ":v1": {
         S: correctIncorrect[0]
        },
        ":v2": {
-         N: "85"
+         N: "80"
        }
      },
     KeyConditionExpression: "label = :v1",
@@ -43,6 +45,7 @@ router.get('/', async (req, res) => {
     return prev.concat(curr);
   })
 
+  //Getting wrong image
   let filenames = filejson.Images;
   let imageSecond = getRandom(filenames, 9-imageCount).map((name) => {
     return {"image": { S: name} }
@@ -65,7 +68,7 @@ router.get('/', async (req, res) => {
     let wrong = true
     for (let j=0; j < dataSecond[i].rekog.L.length; j++) {
       let label = dataSecond[i].rekog.L[j].M 
-      if (label.Name.S === correctIncorrect[0] && label.Confidence.N > 75) {
+      if (label.Name.S === correctIncorrect[0] && label.Confidence.N > 80) {
         wrong = false
         break
       }
@@ -73,18 +76,49 @@ router.get('/', async (req, res) => {
     if (wrong) imageWrongArray.push(dataSecond[i].image.S)
     else imageRandomArray.push(dataSecond[i].image.S)
   }
+
+  //Parsing url
   let linkRandomArray = imageRandomArray.map((name) => {
-    return urllink + name
+    return { link: urllink + name, haveLabel: true } 
   })
   let linkWrongArray = imageWrongArray.map((name) => {
-    return urllink + name
+    return { link: urllink + name, haveLabel: false }
   })
   console.log('CORRECT' ,linkRandomArray)
   console.log('WRONG', linkWrongArray)
-
-  res.send({Correct:linkRandomArray, Wrong:linkWrongArray})
-  //res.render('select', {imageRandomArray})
+  let images = (linkRandomArray.concat(linkWrongArray))
+  res.render('select', { images: shuffle(images), object: correctIncorrect[0]})
 })
+
+router.post('/', async (req, res) => {
+  let questionData = JSON.parse(req.body.question).map((obj) => {
+    return (obj.haveLabel)
+  })
+  let pass = true
+  let answerData = Object.keys(req.body)
+  for (let i=0; i<9; i++) {
+    if(questionData[i] === true && !answerData.includes(i.toString())) {
+      console.log("me ta mai tob ", i)
+      pass = false
+    }
+    if(questionData[i] === false && answerData.includes(i.toString())) {
+      console.log("mai me tae tob", i)
+      pass = false
+    }
+  }
+  if (pass) res.render('writeResult')
+  else res.redirect(req.baseUrl)
+  //res.send(req.body)
+  //res.send(questionData)
+})
+
+function shuffle(a) {
+  for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+  }
+  return a;
+}
 
 function getRandom(arr, n) {
   var result = new Array(n),
